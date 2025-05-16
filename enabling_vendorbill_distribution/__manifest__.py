b0cef6287d13cb6c808e9430614709cea41a39d3
Kuldeep Singh <131156617+kusi-web@users.noreply.github.com>
2025-05-16 16:41:20 +0530

    Update the module

--- enabling_vendorbill_distribution/__manifest__.py
+++ enabling_vendorbill_distribution/__manifest__.py
@@ -1,31 +1,34 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Vendor Bill distribution - Enabling",
-
     'summary': "Vendor Bill distribution - Enabling",
-
     'description': """
         Vendor Bill distribution - Enabling
     """,
-
     'author': "Enabling Ltd",
     'website': "www.enabling.co.nz",
-
     'category': 'Accounting/Accounting',
-    'version': '14',
+    'version': '18.0.1.0.0',
+    'license': 'LGPL-3',
 
-    # any module necessary for this one to work correctly
-    'depends': ['account', 'account_accountant'],
+    'depends': [
+        'account',
+        'account_accountant',
+    ],
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'wizard/distribute_expense_views.xml',
         'views/account_move_views.xml',
-        'views/account_reconcile_model_views.xml'
+        'views/account_reconcile_model_views.xml',
     ],
-    'demo': [],
-    'qweb': ["static/src/xml/account_reconciliation.xml"],
+
+    'assets': {
+        'web.assets_backend': [
+            'enabling_vendorbill_distribution/static/src/xml/account_reconciliation.xml',
+        ],
+    },
+
     'installable': True,
     'application': True,
     'auto_install': False,
