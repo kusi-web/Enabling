7ba82dcc1dd9d8f71d248c29678860c7a3db61b0
Kuldeep Singh <131156617+kusi-web@users.noreply.github.com>
2025-05-19 15:04:05 +0530

    [ADD] add the od journal sequence module
    In this module we added the od journal sequence module which is depended to enabling_credit_note_approve  module

---
+++ od_journal_sequence/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Journal Sequence For Odoo 14',
+    'version': '14.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Journal Sequence For Odoo 14',
+    'live_test_url': 'https://www.youtube.com/watch?v=z-xZwCah7wM',
+    'description': 'Journal Sequence For Odoo 14',
+    'sequence': '1',
+    'author': 'Odoo Developers',
+    'support': 'developersodoo@gmail.com',
+    'depends': ['account'],
+    'demo': [],
+    'data': [
+        'data/account_data.xml',
+        'views/account_journal.xml',
+    ],
+    'qweb': [],
+    'license': 'OPL-1',
+    'price': 1,
+    'currency': 'USD',
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'images': ['static/description/banner.png'],
+}
